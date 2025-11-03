a4ee6b6e5f91ad1aadb42cffaa066779c6634067
Andrealll <39080829+Andrealll@users.noreply.github.com>
2025-11-03 11:47:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-# app.py (root del repo)
 from fastapi import FastAPI
-from app_kb import kb_router 
+from app_kb import kb_router
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"status": "ok", "message": "AstroBot online 🪐"}
-
 
 app.include_router(kb_router)
